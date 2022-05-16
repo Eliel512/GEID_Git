877b0d31baf349f96c82f59b248c7b4866bd08e4
eliel512 <elielmungo9@gmail.com>
2022-05-16 09:09:06 +0000

    Set MONGODB_URI in .env file

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const adminAuth = require('./middleware/adminAuth');
 
 app = express();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017',
+mongoose.connect(process.env.MONGODB_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
